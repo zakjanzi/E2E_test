c585a7d39817df08a4d05208748dc88d4b51bcbe
Zakjanzi <zakjanzi97@gmail.com>
2022-10-14 21:39:09 +0300

    tut test

--- cypress/integration/examples/First_test.js
+++ cypress/integration/examples/First_test.js
@@ -11,8 +11,8 @@ describe("My first test suite", () => {
        
         cy.get('.search-keyword').type('ca')
         cy.wait(2000)
-        // cy.get('.product:visible').should('have.length', 4)
-        // cy.get('.products').find('.product').should('have.length', 4) //parent-child chaining
+        
+        //parent-child chaining
         cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
         // another method without parent-child chaining
         cy.get(':nth-child(3) > .product-action > button')
@@ -23,6 +23,11 @@ describe("My first test suite", () => {
                 cy.wrap($el).contains("ADD TO CART").click()
             }
           })
+
+          cy.get('.brand').then( () => {
+            
+          })
+          cy.log(logo.text())
 
     })
 })
---
+++ cypress/integration/examples/tut_test.js
@@ -0,0 +1,29 @@
+describe('Post Resource', () => {
+    it('Creating a New Post', () => {
+      cy.visit('/posts/new') // 1.
+  
+      cy.get('input.post-title') // 2.
+        .type('My First Post') // 3.
+  
+      cy.get('input.post-body') // 4.
+        .type('Hello, world!') // 5.
+  
+      cy.contains('Submit') // 6.
+        .click() // 7.
+  
+      cy.get('h1') // 8.
+        .should('contain', 'My First Post')
+    })
+  })
+
+
+//This is basically saying:
+
+// 1. Visit page at /posts/new.
+// 2. Find the <input> with class post-title.
+// 3. Type "My First Post" into it.
+// 4. Find the <input> with class post-body.
+// 5. Type "Hello, world!" into it.
+// 6. Find the element containing the text Submit.
+// 7. Click it.
+// 8. Find the h1 tag, ensure it contains the text "My First Post".

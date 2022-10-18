ca9103d55e948ad6e4ebd7417eb1003edf30dce3
Zakjanzi <zakjanzi97@gmail.com>
2022-10-19 00:00:24 +0300

    radio buttons

--- cypress/integration/examples/test4.js
+++ cypress/integration/examples/test4.js
@@ -12,5 +12,19 @@ describe("My first test suite", () => {
         cy.get('#displayed-text').should('not.be.visible')
         cy.get('#show-textbox').click()
         cy.get('#displayed-text').should('be.visible')
+
+
+        // radio buttons section
+        // you can use .check() or .click().
+
+        //First we get the element. The element has a "value" = radio1 (2,3).
+        // To select it, we need to build custom css. (unlike id)
+
+        cy.get("[value=radio2]").check().should('be.checked')
+        //note that u need double quotes here.
+        // so: 1) get it. 2) check/click it. 3) prove it
+
+        
+
     })
 })

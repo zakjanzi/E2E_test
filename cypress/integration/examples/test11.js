5434cf92b6c854eea82a17087aa107619e4ad512
Zakjanzi <zakjanzi97@gmail.com>
2022-10-29 14:58:25 +0300

    intercepting request and mocking response

--- cypress/integration/examples/test11.js
+++ cypress/integration/examples/test11.js
@@ -3,6 +3,9 @@
 describe("Mocking HTTP requests", () => {
 
     it('My 11th test case', () => {
+
+        cy.visit('https://rahulshettyacademy.com/angularAppdemo/')
+
         // we are intercepting a request call that cypress is listening for.
         cy.intercept ({
             method: 'GET',
@@ -13,17 +16,19 @@ describe("Mocking HTTP requests", () => {
             body: 
             [
                 {
-                    "book_name": "null",
-                    "isbn": "SPY40",
-                    "aisle": "2529857"
+                    "book_name": "some random book name",
+                    "isbn": "some random isbn",
+                    "aisle": "some random aisle"
                 },
             ]
+        }).as('bookRetrieval')
+        
+        cy.get('.btn-primary').click()
+        cy.wait('@bookRetrieval').should(({ request, response }) => {
+            cy.get('tr').should('have.length', response.body.length + 1)
         })
-     
-
+        //assertion
+        cy.get('p').should('have.text', 'Oops only 1 Book available')
 
     })
 })
-
-
-//structurte build around mocha testing framework
